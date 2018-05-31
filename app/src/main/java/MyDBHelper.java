a5cf429c3823009be25d0de69d24fa54dbc7058b
cflin <cflin@im.cjcu.edu.tw>
2018-05-31 14:41:20 +0800

    MyDBHelper

---
+++ app/src/main/java/MyDBHelper.java
@@ -0,0 +1,23 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by CJCU on 2018/5/31.
+ */
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
--- app/src/main/java/com/example/cjcu/test0531/AddActivity.java
+++ app/src/main/java/com/example/cjcu/test0531/AddActivity.java
@@ -2,12 +2,25 @@ package com.example.cjcu.test0531;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+    private EditText ed_date;
+    private EditText ed_info;
+    private EditText ed_amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        findViews();
+    }
+
+    private void findViews() {
+        ed_date = findViewById(R.id.ed_date);
+        ed_info = findViewById(R.id.ed_info);
+        ed_amount = findViewById(R.id.ed_amount);
     }
 }
